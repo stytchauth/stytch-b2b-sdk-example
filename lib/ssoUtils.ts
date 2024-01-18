f563a955434872a70b43a40a333656f006b2c9ee
Maxwell Gerber <max@stytch.com>
2024-01-17 16:02:09 -0800

    chore: hardcode live and test envs so we don't need stytch npm package

--- lib/ssoUtils.ts
+++ lib/ssoUtils.ts
@@ -1,9 +1,9 @@
-import * as stytch from 'stytch';
-
 export const publicToken = process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN;
 
-const stytchEnv = process.env.NEXT_PUBLIC_STYTCH_PROJECT_ENV === 'live' ? stytch.envs.live : stytch.envs.test;
+const stytchEnv = process.env.NEXT_PUBLIC_STYTCH_PROJECT_ENV === 'live' ?
+  "https://api.stytch.com/v1/" :
+  "https://test.stytch.com/v1/";
 
 export const formatSSOStartURL = (connection_id: string): string => {
-  return `${stytchEnv}/v1/public/sso/start?connection_id=${connection_id}&public_token=${publicToken}`;
+  return `${stytchEnv}/public/sso/start?connection_id=${connection_id}&public_token=${publicToken}`;
 };
